d62d0364dc6015b630be70597c1276d75951f306
Muhammad Saad <120131320+saadish000@users.noreply.github.com>
2023-06-02 12:53:33 +0500

    Update TopProductTable.js

--- TopProductTable.js
+++ TopProductTable.js
@@ -2,6 +2,8 @@ import { makeStyles } from "@material-ui/core";
 import React, { useState } from "react";
 import { CustomTable } from "../Utils/CustomTable";
 
+//The TopProductTable is getting data from props and then destructing it in dataJson and contains an array of objects.
+
 const TopProductTable = (props) => {
     const { dataJson } = props;
     const columns = [
